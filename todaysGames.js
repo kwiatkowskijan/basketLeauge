597df8a8de07a48754713c4c028472a85f921be6
kwiatkowskijan <kwiatkojan@gmail.com>
2022-01-24 22:11:04 +0100

    mainPage

--- todaysGames.js
+++
@@ -1,8 +0,0 @@
-function hideGames() {
-    var x = document.getElementById("toggle");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
-    } else {
-        x.style.display = "none";
-    }
-}
